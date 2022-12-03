5192968d1acb9cd64212a9330b2d29a44cbf4faf
Artem Semirenko <artemiyka95@gmail.com>
2022-12-03 05:26:21 +0300

    make ShopModule a raw module

--- src/main/java/fenix/product/unlimitedadmin/modules/shop/ShopModule.java
+++ src/main/java/fenix/product/unlimitedadmin/modules/shop/ShopModule.java
@@ -2,7 +2,7 @@ package fenix.product.unlimitedadmin.modules.shop;
 
 import fenix.product.unlimitedadmin.ModulesManager;
 import fenix.product.unlimitedadmin.api.interfaces.ICommand;
-import fenix.product.unlimitedadmin.api.interfaces.module.IModule;
+import fenix.product.unlimitedadmin.api.modules.RawModule;
 import fenix.product.unlimitedadmin.modules.shop.commands.DonateCommand;
 import fenix.product.unlimitedadmin.modules.shop.commands.ShopCommand;
 import fenix.product.unlimitedadmin.modules.shop.data.DonationRepository;
@@ -13,20 +13,27 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ShopModule implements IModule {
+public class ShopModule extends RawModule {
     private final List<ICommand> commands = new ArrayList<>();
     private final List<PlayerDonationCache> donationCaches = new ArrayList<>();
-
-    public ShopModule() {
-        commands.add(new ShopCommand(this));
-        commands.add(new DonateCommand(this));
-        ShopModuleConfig.load();
-        CommandsShopConfig.load();
-    }
 
     @Override
     public @NotNull String getName() {
         return ModulesManager.SHOP.getName();
+    }
+
+
+    @Override
+    public void onEnable() {
+        ShopModuleConfig.load();
+        CommandsShopConfig.load();
+        commands.add(new ShopCommand(this));
+        commands.add(new DonateCommand(this));
+    }
+
+    @Override
+    public void onDisable() {
+
     }
 
     @Override
