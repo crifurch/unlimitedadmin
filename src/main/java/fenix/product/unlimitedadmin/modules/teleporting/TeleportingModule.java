edbfc95c882ea876e516612fc61d10ee3524782f
Artem Semirenko <artemiyka95@gmail.com>
2022-12-03 05:38:23 +0300

    make TeleportingModule a raw module

--- src/main/java/fenix/product/unlimitedadmin/modules/teleporting/TeleportingModule.java
+++ src/main/java/fenix/product/unlimitedadmin/modules/teleporting/TeleportingModule.java
@@ -3,30 +3,39 @@ package fenix.product.unlimitedadmin.modules.teleporting;
 import fenix.product.unlimitedadmin.ModulesManager;
 import fenix.product.unlimitedadmin.UnlimitedAdmin;
 import fenix.product.unlimitedadmin.api.interfaces.ICommand;
-import fenix.product.unlimitedadmin.api.interfaces.module.IModule;
+import fenix.product.unlimitedadmin.api.modules.RawModule;
 import fenix.product.unlimitedadmin.modules.teleporting.commands.TpCommand;
 import fenix.product.unlimitedadmin.modules.teleporting.commands.TpPosCommand;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
-public class TeleportingModule implements IModule {
+public class TeleportingModule extends RawModule {
 
-    private final List<ICommand> commands;
+    private final ArrayList<ICommand> commands = new ArrayList<>();
     final UnlimitedAdmin plugin;
 
     public TeleportingModule(UnlimitedAdmin plugin) {
         this.plugin = plugin;
-        commands = Arrays.asList(
-                new TpCommand(plugin),
-                new TpPosCommand(plugin)
-        );
+
     }
 
     @Override
     public @NotNull String getName() {
         return ModulesManager.TELEPORT.getName();
+    }
+
+
+    @Override
+    public void onEnable() {
+        commands.add(new TpCommand(plugin));
+        commands.add(new TpPosCommand(plugin));
+    }
+
+    @Override
+    public void onDisable() {
+
     }
 
     @Override
