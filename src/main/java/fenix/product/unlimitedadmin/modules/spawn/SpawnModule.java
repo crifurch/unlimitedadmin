9c62e57b74e2a1d2b33054024af6cb33aca88fc4
Artem Semirenko <artemiyka95@gmail.com>
2022-12-03 05:32:59 +0300

    make SpawnModule a raw module

--- src/main/java/fenix/product/unlimitedadmin/modules/spawn/SpawnModule.java
+++ src/main/java/fenix/product/unlimitedadmin/modules/spawn/SpawnModule.java
@@ -3,8 +3,7 @@ package fenix.product.unlimitedadmin.modules.spawn;
 import fenix.product.unlimitedadmin.ModulesManager;
 import fenix.product.unlimitedadmin.UnlimitedAdmin;
 import fenix.product.unlimitedadmin.api.interfaces.ICommand;
-import fenix.product.unlimitedadmin.api.interfaces.module.IModule;
-import fenix.product.unlimitedadmin.api.utils.FileUtils;
+import fenix.product.unlimitedadmin.api.modules.RawModule;
 import fenix.product.unlimitedadmin.api.utils.PlayerUtils;
 import fenix.product.unlimitedadmin.modules.spawn.commands.DelSpawnCommand;
 import fenix.product.unlimitedadmin.modules.spawn.commands.ListSpawnCommand;
@@ -14,39 +13,66 @@ import fenix.product.unlimitedadmin.modules.spawn.listeners.SpawnDeathListener;
 import fenix.product.unlimitedadmin.modules.spawn.listeners.SpawnLoginListener;
 import org.bukkit.Location;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.event.Listener;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
-public class SpawnModule implements IModule {
+public class SpawnModule extends RawModule {
 
     private static YamlConfiguration cfg;
     private static File f;
     final UnlimitedAdmin plugin;
     private final List<ICommand> commands = new ArrayList<>();
+    private final List<Listener> listeners = new ArrayList<>();
 
     public SpawnModule(UnlimitedAdmin plugin) {
         this.plugin = plugin;
-        f = FileUtils.getFileFromList(UnlimitedAdmin.getInstance().getModuleFolder(this), Collections.singletonList("spawns.yml"));
+        f = plugin.getModuleConfigFile(this, "spawns");
+
+
+    }
+
+    @Override
+    public @NotNull String getName() {
+        return ModulesManager.SPAWN.getName();
+    }
+
+    @Override
+    public void onEnable() {
         cfg = YamlConfiguration.loadConfiguration(f);
         SpawnModuleConfig.load();
         commands.add(new SetSpawnCommand(this));
         commands.add(new SpawnCommand(this));
         commands.add(new DelSpawnCommand(this));
         commands.add(new ListSpawnCommand(this));
+
         if (SpawnModuleConfig.PREFER_TELEPORT_ON_DEATH.getBoolean()) {
-            plugin.getServer().getPluginManager().registerEvents(new SpawnDeathListener(this), plugin);
+            listeners.add(new SpawnDeathListener(this));
         }
         if (SpawnModuleConfig.PREFERRED_FIRST_SPAWN.getBoolean()) {
-            plugin.getServer().getPluginManager().registerEvents(new SpawnLoginListener(this), plugin);
+            listeners.add(new SpawnLoginListener(this));
         }
     }
 
     @Override
-    public @NotNull String getName() {
-        return ModulesManager.SPAWN.getName();
+    public void onDisable() {
+
+    }
+
+    @Override
+    public @NotNull List<ICommand> getCommands() {
+        return commands;
+    }
+
+    @Override
+    public List<Listener> getListeners() {
+        return listeners;
     }
 
     public void setSpawn(@NotNull String name, @NotNull Location position) {
@@ -88,9 +114,4 @@ public class SpawnModule implements IModule {
         return false;
     }
 
-
-    @Override
-    public @NotNull List<ICommand> getCommands() {
-        return commands;
-    }
 }
