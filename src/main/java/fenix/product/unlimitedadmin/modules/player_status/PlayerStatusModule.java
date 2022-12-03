d45e57622f5c3f0f174885c94b9b351ee952220a
Artem Semirenko <artemiyka95@gmail.com>
2022-12-03 05:17:03 +0300

    make PlayerStatusModule a raw module

--- src/main/java/fenix/product/unlimitedadmin/modules/player_status/PlayerStatusModule.java
+++ src/main/java/fenix/product/unlimitedadmin/modules/player_status/PlayerStatusModule.java
@@ -3,28 +3,21 @@ package fenix.product.unlimitedadmin.modules.player_status;
 import fenix.product.unlimitedadmin.ModulesManager;
 import fenix.product.unlimitedadmin.UnlimitedAdmin;
 import fenix.product.unlimitedadmin.api.interfaces.ICommand;
-import fenix.product.unlimitedadmin.api.interfaces.module.IModule;
+import fenix.product.unlimitedadmin.api.modules.RawModule;
 import fenix.product.unlimitedadmin.modules.player_status.commands.*;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class PlayerStatusModule implements IModule {
+public class PlayerStatusModule extends RawModule {
 
     private final List<ICommand> commands = new ArrayList<>();
     final UnlimitedAdmin plugin;
 
     public PlayerStatusModule(UnlimitedAdmin plugin) {
         this.plugin = plugin;
-        this.commands.add(new HealCommand());
-        this.commands.add(new HealAllCommand());
-        this.commands.add(new KillCommand());
-        this.commands.add(new KillAllCommand());
-        this.commands.add(new FeedCommand());
-        this.commands.add(new FeedAllCommand());
-        this.commands.add(new FlyCommand());
-        this.commands.add(new GetPosCommand());
+
     }
 
     @Override
@@ -33,7 +26,26 @@ public class PlayerStatusModule implements IModule {
     }
 
     @Override
+    public void onEnable() {
+        commands.add(new HealCommand());
+        commands.add(new HealAllCommand());
+        commands.add(new KillCommand());
+        commands.add(new KillAllCommand());
+        commands.add(new FeedCommand());
+        commands.add(new FeedAllCommand());
+        commands.add(new FlyCommand());
+        commands.add(new GetPosCommand());
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    @Override
     public @NotNull List<ICommand> getCommands() {
         return commands;
     }
+
+
 }
