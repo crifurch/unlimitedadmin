c61fe3fb898cd71faf4c807f1346699369e5f273
Artem Semirenko <artemiyka95@gmail.com>
2022-12-03 05:40:01 +0300

    make TabListModule extends AdminModule

--- src/main/java/fenix/product/unlimitedadmin/modules/tablist/TabListModule.java
+++ src/main/java/fenix/product/unlimitedadmin/modules/tablist/TabListModule.java
@@ -1,8 +1,7 @@
 package fenix.product.unlimitedadmin.modules.tablist;
 
 import fenix.product.unlimitedadmin.ModulesManager;
-import fenix.product.unlimitedadmin.UnlimitedAdmin;
-import fenix.product.unlimitedadmin.api.interfaces.module.IModule;
+import fenix.product.unlimitedadmin.api.modules.AdminModule;
 import fenix.product.unlimitedadmin.api.utils.PlaceHolderUtils;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -10,11 +9,12 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Collection;
+import java.util.Collections;
+
 // todo: support icons
-public class TabListModule implements IModule, Listener {
-    public TabListModule(UnlimitedAdmin plugin) {
-        TabListConfig.load();
-        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+public class TabListModule extends AdminModule implements Listener {
+    public TabListModule() {
     }
 
     @Override
@@ -22,9 +22,29 @@ public class TabListModule implements IModule, Listener {
         return ModulesManager.TABLIST.getName();
     }
 
+
+    @Override
+    public void onEnable() {
+        TabListConfig.load();
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    @Override
+    public Collection<Listener> getListeners() {
+        return Collections.singletonList(this);
+    }
+
     @EventHandler
     public void onPlayerLogin(PlayerLoginEvent event) {
         final Player player = event.getPlayer();
+        updateTabList(player);
+    }
+
+    private void updateTabList(Player player) {
         final String formatedText = PlaceHolderUtils.replacePlayerPlaceholders(
                 TabListConfig.TABLIST_FORMAT.getString());
         player.setPlayerListName(formatedText);
